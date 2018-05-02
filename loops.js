a9a5c35707abd7c9f0da8a1e40ba65d23501f628
Sean Logan Para <spara77@gmail.com>
2018-05-02 19:37:57 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -21,7 +21,7 @@ function maybeTrue() {
 function doWhileLoop(array) {do {
   array.length-1
 } 
-while(array.length > 0 && maybeTrue() ); 
+while(array.length-1 > 0 && maybeTrue() ); 
 return array
 }
 
